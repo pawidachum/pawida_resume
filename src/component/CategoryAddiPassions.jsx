0913f458c4a452e98f6f6e69a397e356714955c9
pawida chumpurat <pawida.may@gmail.com>
2023-03-08 21:58:18 +0700

    update: additional passion rearange

--- src/component/CategoryAddiPassions.jsx
+++ src/component/CategoryAddiPassions.jsx
@@ -2,17 +2,19 @@
 function CategoryAdditionalPassion(props){
     const additionalPassion = ["Front-End", "Back-End", "UI/UX"]
     return(
-        <div className="work-experience">
-            {additionalPassion.map((passion, index)=>{
-                return(
-                    <>
+        <>
+        {additionalPassion.map((passion, index)=>{
+            return(
+                <>
+                <div className="work-experience">
                     <div key={index}>image</div>
                     <div><strong>{passion}</strong>Developer</div>
                     <p>(Bootstrap, Tailwind)</p>
+                    </div>
                     </>
-                )
-            })}
-        </div>
+                    )
+                })}
+        </>
     )
     
 };
