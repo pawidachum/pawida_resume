cb2f541e3bfef1de958ba12c88e7ccc390122341
pawida chumpurat <pawida.may@gmail.com>
2023-04-14 16:41:11 +0700

    add(index.html): install tailwind

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function NavBar(){
     return (
         <div className="navbar">
-                <div><Link className="profile-link" to="/"><strong>Pawida</strong> Chumpurat</Link></div>
+                <div><Link className="" to="/"><strong>Pawida</strong> Chumpurat</Link></div>
                 <div className="nav-link">
                     <Link className="about-link" to="/aboutme">About</Link>
                     <Link className="passions-link" to="/passions">Passions</Link>
