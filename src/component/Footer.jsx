caffb4d61cbe82867ba72c628bbbcc407ce7c2a0
pawida chumpurat <pawida.may@gmail.com>
2023-04-16 20:28:08 +0700

    style: add style in Footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -2,9 +2,9 @@ import waveImage from "../assets/decorate/wave.png"
 
 function Footer(){
     return (
-        <div>
-            <img className="" src={waveImage} alt="wave-image"/>
-            <div className="">Design By Pawida</div>
+        <div className="relative">
+            <img className="relative w-full z-0" src={waveImage} alt="wave-image"/>
+            <div className="text-white text-4xl absolute bottom-7 inset-x-1/3 text-center tracking-wider z-10">Design By Pawida</div>
         </div>
     )
 };
